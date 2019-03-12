569cb3a7cc251419e2beae44619eb364a1b637ea
Liviu Popa <liviucst@gmail.com>
2019-03-12 13:24:20 +0000

    Created test suite for StringMatrix

---
+++ MatrixTests/TypeDependentTests/StringMatrixTests/tst_stringmatrixtests.cpp
@@ -0,0 +1,100 @@
+#include <QtTest>
+
+#include "../../../MatrixLib/matrix.h"
+
+using StringMatrix = Matrix<std::string>;
+
+class StringMatrixTests : public QObject
+{
+    Q_OBJECT
+
+public:
+    StringMatrixTests();
+    ~StringMatrixTests();
+
+private slots:
+    void testMatrixesAreEqual();
+    void testMatrixesAreNotEqual();
+
+};
+
+StringMatrixTests::StringMatrixTests()
+{
+
+}
+
+StringMatrixTests::~StringMatrixTests()
+{
+
+}
+
+void StringMatrixTests::testMatrixesAreEqual()
+{
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+
+        QVERIFY2(firstMatrix == secondMatrix && secondMatrix == firstMatrix, "Matrixes are not equal");
+    }
+
+    {
+        StringMatrix matrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+
+        QVERIFY2(matrix == matrix, "The matrix is not equal to itself");
+    }
+
+    {
+        StringMatrix firstMatrix{};
+        StringMatrix secondMatrix{};
+
+        QVERIFY2(firstMatrix == secondMatrix && secondMatrix == firstMatrix, "Matrixes are not equal");
+    }
+
+    {
+        StringMatrix matrix{};
+
+        QVERIFY2(matrix == matrix, "The matrix is not equal to itself");
+    }
+}
+
+void StringMatrixTests::testMatrixesAreNotEqual()
+{
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{2, 3, {"abc", "abde", "-abc", "Abc", "1abc", "-0.8333"}};
+
+        QVERIFY2(firstMatrix != secondMatrix && secondMatrix != firstMatrix, "Matrixes are equal");
+    }
+
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{2, 2, {"abc", "abdc", "Abc", "1abc"}};
+
+        QVERIFY2(firstMatrix != secondMatrix && secondMatrix != firstMatrix, "Matrixes are equal");
+    }
+
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{3, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333", "Abc", "c1D", "-A1bE2"}};
+
+        QVERIFY2(firstMatrix != secondMatrix && secondMatrix != firstMatrix, "Matrixes are equal");
+    }
+
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{3, 2, {"abc", "abdc", "Abc", "1abc", "-abc", "-0.8333"}};
+
+        QVERIFY2(firstMatrix != secondMatrix && secondMatrix != firstMatrix, "Matrixes are equal");
+    }
+
+    {
+        StringMatrix firstMatrix{2, 3, {"abc", "abdc", "-abc", "Abc", "1abc", "-0.8333"}};
+        StringMatrix secondMatrix{};
+
+        QVERIFY2(firstMatrix != secondMatrix && secondMatrix != firstMatrix, "Matrixes are equal");
+    }
+}
+
+QTEST_APPLESS_MAIN(StringMatrixTests)
+
+#include "tst_stringmatrixtests.moc"
--- MatrixTests/TypeDependentTests/TypeDependentTests.pro
+++ MatrixTests/TypeDependentTests/TypeDependentTests.pro
@@ -4,4 +4,5 @@ SUBDIRS += \
     IntMatrixTests \
     DecimalMatrixTests \
     FractionMatrixTests \
-    QPointFMatrixTests
+    QPointFMatrixTests \
+    StringMatrixTests
